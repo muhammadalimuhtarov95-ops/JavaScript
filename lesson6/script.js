fab81571fba244d19c8741aee0721729968e6e06
muhammadalimuhtarov95-ops <muhammadalimuhtarov95@gmail.com>
2026-04-03 10:38:36 +0500

    git buyruqlari darsi

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,1 +1,27 @@
 console.log("Github")
+
+//git init
+
+//git remote add origin https://repozitory-manzili
+
+//git add .
+
+//git commit -m "comenteria yoziladi"
+
+//git push origin master*
+
+//========================================
+
+//qo'shimcha so'rab qolsa
+
+//git config --global user.emeil "muhammadalimuhtarov95"
+//git config --global user.name "muhammadalimuhtarov95-ops"
+
+//=======================================
+
+
+//doim yangi cod yozsak 3 ta git buyrug'ini doim ishlatishimmiz shart
+
+//git add . 
+//git commit -m "kamentarea yozing"
+//git push origin master*
