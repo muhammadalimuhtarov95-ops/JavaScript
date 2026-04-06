1cbe799310a3fdfcab483194e6056b3e6abeffeb
muhammadalimuhtarov95-ops <muhammadalimuhtarov95@gmail.com>
2026-04-06 10:33:38 +0500

    For Loop

---
+++ lesson7/script.js
@@ -0,0 +1,36 @@
+// // For Loop 
+
+// // start = 1
+// // stop = 10
+
+// // for (let raqam = 1; raqam <=10; raqam++){
+// //     console.log(raqam)
+// // }
+
+// // console.log("=========================")
+
+// // for (let raqam = 1; raqam <=10; raqam++){
+// //     console.log(raqam)
+// // }
+
+// // console.log("=========================")
+
+// // for (let raqam = 10; raqam >=1; raqam--){
+// //     console.log(raqam)
+// // }
+
+// // console.log("=========================")
+
+// // for (let raqam = 10; raqam >=1; raqam-=2){
+// //     console.log(raqam)
+// // }
+
+
+// // start = 1
+// // stop = 10
+
+// for (let raqam = 1; raqam <= 10; raqam++) {
+//     if (raqam % 2 === 0) {
+//         console.log(raqam)
+//     }
+// }
