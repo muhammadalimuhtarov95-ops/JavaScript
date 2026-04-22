f203fc3c83240368bea85ffd7f66dd06aa0ef001
muhammadalimuhtarov95-ops <muhammadalimuhtarov95@gmail.com>
2026-04-22 09:48:19 +0500

    object o'rgandik

---
+++ lesson15/script.js
@@ -0,0 +1,46 @@
+// // 1. Decloration Function
+// // 2. Exproression Function
+// // 3. Arrow Function
+
+// let a = 1
+// let b = 2
+// let c = 3
+// function eshmat(r1, r2, r3) {
+//     let summa = r1 + r2 + r3;
+//     return summa
+// }
+// let sum = eshmat(a, b, c);
+// console.log(sum / 2)
+
+
+
+// const summa2 = (r1, r2, r3) => {
+//     let summa = r1 + r2 + r3;
+//     return summa
+// }
+// let sum2 = eshmat(a, b, c);
+// console.log(sum2 / 2)
+
+
+
+// const summa = function (r1, r2, r3) {
+//     let summa = r1 + r2 + r3;
+//     return summa
+// }
+// let sum3 = eshmat(a, b, c);
+// console.log(sum3 / 2)
+
+
+// let obj = {
+//     ism:"Azizbeeee",
+//     Familia:"Abdukatimov",
+//     Mahalla:"luli mahalla",
+//     isGoSchool:true
+// }
+// // 1. Static usul
+// console.log(obj.ism, obj.Familia)
+
+// // 2.Dinamic usul
+// console.log(obj["ism"], obj["Familia"]);
+
+
