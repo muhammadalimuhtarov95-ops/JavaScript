3f0d81670098a79874aca5a9b8f60b8a6386c9af
muhammadalimuhtarov95-ops <muhammadalimuhtarov95@gmail.com>
2026-05-25 09:59:03 +0500

    Methodslar

---
+++ 2 Bo'lim/lesson4'/script.js
@@ -0,0 +1,73 @@
+// // // let obj = {
+// // //     id: 1,
+// // //     ism:"Ali",
+// // //     familiya:"Valiyev",
+// // //     manzil:"Namangan",
+// // // };
+
+// // // console.log(obj.child?.ism);
+// // // console.log(obj["chile"]?.["ism"]);
+
+// // // =====================================
+
+// // //  STRING
+
+// // // 1. "" 
+
+// // // 2. ''
+
+// // // 3. ``
+
+// // // 4. String()
+
+// // // 5. new String() - Constractor 
+
+// // let str1 = "O'zbekiston";
+// // let str2 = 'O`zbekiston'
+// // let str3 = `O'zbekiston`
+// // let str4 =String("Ozbekistnm")
+
+// // // ===============
+// // let str = "Algoritmbo'laklatfggjdgfjtsnfggfshfgegfhsgfhdfhddshjsdhjfsajjfjddgjdsfg";
+// // // console.log(str);
+// // // console.log(str[1])
+
+// // // for in - object
+// // // for of - String
+
+// // for(let value of str){
+// //     console.log(value)
+// // }
+
+// // let str = "Algoritm";
+
+// // // console.log(str.length)  // 8
+// // // console.log(str[0]); A
+// // // console.log(str.charAt(0))
+// // // console.log(str.at(-1))
+
+// // console.log(str[0].charCodeAt())
+// // console.log(str.charCodeAt(2))
+
+// // let a = "salom"
+// // let a = "Alik"
+// // console.log(a + b);
+// // console.log(addEventListener.cocat(b))
+
+// // incloud
+// // let str = "Algoritm";
+// // console.log(str.includes(Ag)); // false
+// // console.log(str.includes(A)); // true
+// // startsWith()
+// // console.log(str.startsWith("Alg")) // true
+// // endsWith
+// // console.log(str.endsWith("tm")) // true
+
+// let str = "AlgoritmA5";
+
+// // // searech
+// // console.log(str.search("A"))// Regex
+// // console.log(str.indexOf("A", 1));
+// // console.log(str.lastIndexOf("A",5));
+
+// console.log(str.search(/5/d))
