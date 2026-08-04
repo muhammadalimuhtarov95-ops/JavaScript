ec34341631220931f3e9c5388799a1a142af9b21
muhammadalimuhtarov95-ops <muhammadalimuhtarov95@gmail.com>
2026-08-04 15:46:09 +0500

    vazifalar

---
+++ 3 bo'lim/4/script.js
@@ -0,0 +1,29 @@
+// let arr = ["Olma", "Anor", "Uzum", "Nok"]
+// let len = arr.find((value, index, a) => {
+//     if(value === "Uzum") return value
+// })
+// console.log(len.length)
+
+// let arr = [5, 2, 7, 9, 5, 10, 1, 4]
+// let katta = Math.max(...arr)
+// let max = arr.findIndex((value, index, a) => {
+//     if(katta === value) return value
+// })
+// console.log(max)
+
+// let arr = ["Olma", "Anor", "Uzum", "Nok"];
+// let len = 0;
+// let v = 0
+// let a = arr.forEach((value, index, a) => {
+//     if(value.length > len){
+//         len = value.length
+//         v = value
+//     }
+// });
+// console.log(v)
+
+// let arr = ["Screatch", "Paython", "Html", "css", "Screatch", "Js", "React Js"];
+// let a = arr.findLastIndex((value, index, a) => {
+//     return value === "Screatch"
+// })
+// console.log(a)
